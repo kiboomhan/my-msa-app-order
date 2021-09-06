0663048fa9afc02210574120cb784bd8820feb1f
kiboomhan <kiboomhan@gmail.com>
2021-09-06 17:13:22 +0900

    Initial commit

---
+++ src/main/java/com/example/msaapporder/MsaAppOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.msaapporder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaAppOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaAppOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/msaapporder/MsaAppOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msaapporder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaAppOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
